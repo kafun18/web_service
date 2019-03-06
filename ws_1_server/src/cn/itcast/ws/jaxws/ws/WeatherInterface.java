fb5abd69a9395a151e01253800af48b42189989c
kafun18 <513112374@qq.com>
2019-03-06 22:59:55 +0800

    服务端

---
+++ ws_1_server/src/cn/itcast/ws/jaxws/ws/WeatherInterface.java
@@ -0,0 +1,16 @@
+package cn.itcast.ws.jaxws.ws;
+
+/**
+ * 
+ * <p>Title: WeatherInterface.java</p>
+ * <p>Description:SEI接口</p>
+ * <p>Company: www.itcast.com</p>
+ * @author  传智.at
+ * @date    2015年11月26日上午9:28:00
+ * @version 1.0
+ */
+public interface WeatherInterface {
+
+	public String queryWeather(String cityName);
+	
+}
